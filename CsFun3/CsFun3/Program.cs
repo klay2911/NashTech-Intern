04ea5335c704f0e79b3938b07572bdb903203c93
klay2911 <klaynob2911@email.com>
2023-06-20 16:07:38 +0700

    V5
    Add CsFun3 and CsFun3_Clock

---
+++ CsFun3/CsFun3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace CsFun3;
+class Program
+{
+        static async Task Main(string[] args)
+        {
+                //return the total prime numbers and process time
+                var sw = new Stopwatch();
+                sw.Start();
+                var primes = await GetPrimesAsync(2, 10000000);
+                sw.Stop();
+                Console.WriteLine("Total prime numbers: {0}\nProcess time: {1}", primes.Count, sw.Elapsed.TotalSeconds);
+                
+                //return prime numbers for a range (e.g. from 0 to 100)
+                
+                /*
+                int start = 0;
+                int end = 100;
+
+                List<int> result = await GetPrimesAsync(start, end);
+                
+                Console.WriteLine("Prime numbers between {0} and {1}:", start, end );
+                foreach (int prime in result)
+                {
+                        Console.Write(prime + " ");
+                }
+                */
+
+        }
+
+        static async Task<List<int>> GetPrimesAsync(int start, int end)
+        {
+                var result = new List<int>();
+
+                //List<int> result = new List<int>();
+
+                await Task.Run(() =>
+                {
+                        for (int i = start; i <= end; i++)
+                        {
+                                if (IsPrime(i))
+                                {
+                                        result.Add(i);
+                                }
+                        }
+                });
+
+                return result;
+        }
+
+        static bool IsPrime(int number)
+        {
+                if (number % 2 == 0)
+                {
+                        return number == 2;
+                }
+                else
+                {
+                        var topLimit = (int)Math.Sqrt(number);
+                        for (int i = 3; i <= topLimit; i += 2)
+                        {
+                                if (number % i == 0) return false;
+                        }
+
+                        return true;
+                }
+        }
+        /*
+         static bool IsPrime(int number)
+        {
+                if (number < 2) return false;
+                if (number == 2) return true;
+                if (number % 2 == 0) return false;
+
+                var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+                for (int i = 3; i <= boundary; i += 2)
+                        if (number % i == 0)
+                                return false;
+
+                return true;
+        }
+        */
+}
+
---
+++ CsFun3_Clock/CsFun3_Clock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CsFun3_Clock;
+class Program
+{
+    public class Publisher
+    {
+        public event Action OnChange = delegate { };
+
+        public void Raise()
+        {
+            OnChange();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Khởi tạo publisher
+        var pub = new Publisher();
+
+        pub.OnChange += () => Console.WriteLine(DateTime.Now.ToString("T"));
+        for (int i = 0; i < 1000; ++i)
+        {
+            pub.Raise();
+            Thread.Sleep(1000);
+        }
+    }
+}
